98477d2e6d1120d140788ade7afce368ca07037c
Felix Mertineit <felix.mertineit@aureka.ai>
2021-09-09 12:18:19 +0200

    [mod] enable users and login namespaces including routes

--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -12,7 +12,7 @@ from app.utils import send_new_account_email
 
 router = APIRouter()
 
-
+"""
 @router.get("/", response_model=List[schemas.User])
 def read_users(
     db: Session = Depends(deps.get_db),
@@ -20,9 +20,9 @@ def read_users(
     limit: int = 100,
     current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
-    """
+    \"""
     Retrieve users.
-    """
+    \"""
     users = crud.user.get_multi(db, skip=skip, limit=limit)
     return users
 
@@ -34,9 +34,9 @@ def create_user(
     user_in: schemas.UserCreate,
     current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
-    """
+    \"""
     Create new user.
-    """
+    \"""
     user = crud.user.get_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
@@ -49,7 +49,7 @@ def create_user(
             email_to=user_in.email, username=user_in.email, password=user_in.password
         )
     return user
-
+"""
 
 @router.put("/me", response_model=schemas.User)
 def update_user_me(
@@ -85,43 +85,16 @@ def read_user_me(
     """
     return current_user
 
-
-@router.post("/open", response_model=schemas.User)
-def create_user_open(
-    *,
-    db: Session = Depends(deps.get_db),
-    password: str = Body(...),
-    email: EmailStr = Body(...),
-    full_name: str = Body(None),
-) -> Any:
-    """
-    Create new user without the need to be logged in.
-    """
-    if not settings.USERS_OPEN_REGISTRATION:
-        raise HTTPException(
-            status_code=403,
-            detail="Open user registration is forbidden on this server",
-        )
-    user = crud.user.get_by_email(db, email=email)
-    if user:
-        raise HTTPException(
-            status_code=400,
-            detail="The user with this username already exists in the system",
-        )
-    user_in = schemas.UserCreate(password=password, email=email, full_name=full_name)
-    user = crud.user.create(db, obj_in=user_in)
-    return user
-
-
+"""
 @router.get("/{user_id}", response_model=schemas.User)
 def read_user_by_id(
     user_id: int,
     current_user: models.User = Depends(deps.get_current_active_user),
     db: Session = Depends(deps.get_db),
 ) -> Any:
-    """
+    \"""
     Get a specific user by id.
-    """
+    \"""
     user = crud.user.get(db, id=user_id)
     if user == current_user:
         return user
@@ -140,9 +113,9 @@ def update_user(
     user_in: schemas.UserUpdate,
     current_user: models.User = Depends(deps.get_current_active_superuser),
 ) -> Any:
-    """
+    \"""
     Update a user.
-    """
+    \"""
     user = crud.user.get(db, id=user_id)
     if not user:
         raise HTTPException(
@@ -151,3 +124,4 @@ def update_user(
         )
     user = crud.user.update(db, db_obj=user, obj_in=user_in)
     return user
+"""
