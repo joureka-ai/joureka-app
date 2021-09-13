5397bc2be8fd9b4f9afd36efbd9b4ed9a34a7fa5
Ana-Maria Tomi <33226694+anatomi@users.noreply.github.com>
2021-09-13 10:23:55 +0200

    add custom theme

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
-
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../utils/theme';
 import "../styles/global.scss";
+import Button from "@material-ui/core/Button";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="success">
+        Primary
+      </Button>
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
