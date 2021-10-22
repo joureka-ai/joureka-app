f55b7957d2bf60b9e8d1614ee8ed3c425068d9e5
Felix Mertineit <felix.mertineit@aureka.ai>
2021-10-22 14:50:44 +0200

    [mod] update the logger

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -4,9 +4,17 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+from pathlib import Path
+import logging
+
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
+# setup loggers
+logging_conf = Path(__file__).parent / "logging.conf"
+assert logging_conf.is_file()
+logging.config.fileConfig(logging_conf, disable_existing_loggers=False)
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
