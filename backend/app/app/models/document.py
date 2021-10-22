07ec73793047a695ddebfc72b56f38d3ca3c6e51
Felix Mertineit <felix.mertineit@aureka.ai>
2021-10-22 14:57:36 +0200

    [mod] models for representing annotations

--- backend/app/app/models/document.py
+++ backend/app/app/models/document.py
@@ -1,13 +1,14 @@
 """ORM model for a document in the system"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey, Index
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, deferred
 from sqlalchemy_utils import TSVectorType
 
 from app.db.base_class import Base
 
 from .transcript.aws import AWSTranscription
 from .word import Word
+from .annot import Annot
 
 
 class Document(Base):
@@ -31,11 +32,15 @@ class Document(Base):
     fk_aws_transcription = Column(Integer, ForeignKey("aws_transcription.id"))
     transcription = relationship(AWSTranscription)
 
-    fulltext = Column(String, nullable=True)
-    fulltext_search_vector = Column(TSVectorType("fulltext"))
-    fulltext_regconfig = Column(String, nullable=True)
+    fulltext = deferred(Column(String, nullable=True))
+    fulltext_search_vector = deferred(Column(TSVectorType("fulltext")))
+    fulltext_regconfig = deferred(Column(String, nullable=True))
 
+    annotations = relationship(Annot, cascade="all, delete-orphan")
     words = relationship(Word, cascade="all, delete-orphan", order_by=Word.order)
     fk_project = Column(Integer, ForeignKey("project.id"))
 
+
+
 Word.document = relationship(Document, back_populates="words")
+Annot.document = relationship(Document, back_populates="annotations")
