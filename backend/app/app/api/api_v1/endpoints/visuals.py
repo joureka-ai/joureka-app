1e862e6b3d468007fec9a0f21a931358b6cf6854
Felix Mertineit <fmertineit@gmail.com>
2021-12-09 15:05:55 +0100

    [add]  endpoint for topiccloud

--- backend/app/app/api/api_v1/endpoints/visuals.py
+++ backend/app/app/api/api_v1/endpoints/visuals.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from app import models, crud, schemas
 from app.api import deps
 from jouresearch_nlp.wordcloud.frequency import calculate_freq_over_docs
-from .helper import check_doc_in_docs, calcuate_frequencies, transform_word_freqs
+from .helper import check_doc_in_docs, calcuate_frequencies, transform_word_freqs, gather_annots_by_type
 import logging
 
 router = APIRouter()
@@ -48,36 +48,45 @@ def generate_frequencies(
     return transform_word_freqs(freq_list)
 
 @router.get('/{project_id}/pinplot/')
-def get_frequencies(
+def get_pin_frequencies(
     project_id: int,
     document_id: Optional[int] = None,
     current_user: models.User = Depends(deps.get_current_active_user),
     db: Session = Depends(deps.get_db)
-    ) -> schemas.PinFrequencies:
+    ) -> schemas.AnnotFrequencies:
     """
     Generate the frequencies pins in either a single document or in all of the documents of a project.
     - Args: Project ID and the OPTIONAL document ID
     - Returns: The pins with frequency and corresponding document.
     """
 
-    if document_id:
-        pins = crud.annot.get_by_d_type(db, document_id, type="Pin")
+    pins = gather_annots_by_type(db, project_id, "Pin", document_id)
     
-    if not document_id:
-        # Get all pins in the database
-        pins = crud.annot.get_all_by_type(db, type="Pin")
-
-        LOG.info(pins)
-        # get all documents of a project
-        docs = crud.document.get_all_by_p_id(db, project_id)
-        doc_ids = [doc.id for doc in docs]
-        
-        # Make sure to only take pins that are in the project
-        pins = [pin for pin in pins if check_doc_in_docs(doc_ids, pin.fk_document)]
-
     if not pins:
         raise HTTPException(
             status_code=404, detail="There are no pins for this document or the document is not in the given project."
         )
 
     return calcuate_frequencies(pins)
+
+@router.get('/{project_id}/topicplot/')
+def get_topic_frequencies(
+    project_id: int,
+    document_id: Optional[int] = None,
+    current_user: models.User = Depends(deps.get_current_active_user),
+    db: Session = Depends(deps.get_db)
+    ) -> schemas.AnnotFrequencies:
+    """
+    Generate the frequencies topics in either a single document or in all of the documents of a project.
+    - Args: Project ID and the OPTIONAL document ID
+    - Returns: The topics with frequency and corresponding document.
+    """
+
+    topics = gather_annots_by_type(db, project_id, "Topic", document_id)
+    
+    if not topics:
+        raise HTTPException(
+            status_code=404, detail="There are no Topics for this document or the document is not in the given project."
+        )
+
+    return calcuate_frequencies(topics)
