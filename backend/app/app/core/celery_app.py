572338ed17084b643ddd24c15f7bef8e94dd10fd
Felix Mertineit <fmertineit@gmail.com>
2022-02-17 11:20:52 +0100

    [mod] celery app to use AMQP instead of RPC backend for task status

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,5 +1,8 @@
 from celery import Celery
 from celery.concurrency import asynpool
 
+# Use AMQP backend as otherwise the result status are not stored
+celery_app = Celery("worker", backend="amqp://", broker="amqp://guest@queue//", include=["app.worker.transcribe"])
 
-celery_app = Celery("worker", backend="rpc://", broker="amqp://guest@queue//", include=["app.worker.transcribe"])
+default_config = "app.worker.celeryconfig"
+celery_app.config_from_object(default_config)
