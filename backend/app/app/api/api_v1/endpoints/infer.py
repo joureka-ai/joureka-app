1b7197d1ce196b44e17cd2702ba69ea369ad86e7
Felix Mertineit <fmertineit@gmail.com>
2022-02-17 11:20:52 +0100

    [mod] celery app to use AMQP instead of RPC backend for task status

--- backend/app/app/api/api_v1/endpoints/infer.py
+++ backend/app/app/api/api_v1/endpoints/infer.py
@@ -12,15 +12,16 @@ from app.file_storage import get_transcript_key
 
 router = APIRouter()
 
-@router.post('/{project_id}/docs/{document_id}', status_code=201)
+
+@router.post("/{project_id}/docs/{document_id}", status_code=201)
 def transcribe(
     project_id: int,
     document_id: int,
     background_task: BackgroundTasks,
     engine: Engines = Engines.vosk,
     current_user: models.User = Depends(deps.get_current_active_superuser),
-    db: Session = Depends(deps.get_db)
-    ) -> Any:
+    db: Session = Depends(deps.get_db),
+) -> Any:
     document = crud.document.get_by_p_id(db, document_id, project_id)
 
     if not document:
@@ -30,29 +31,35 @@ def transcribe(
 
     audio_file_key = document.audio_file_key
     assert audio_file_key
-    
+
     if document.fulltext:
         raise HTTPException(
             status_code=409, detail="There is already an existing transcript!"
         )
     
-    
+    #celery_app.Task.track_started = True
+
     task = celery_app.send_task(
         name=f"worker.transcribe.{engine}",
         args=[audio_file_key, document_id, project_id, engine],
-        queue="main-queue")
+        queue="main-queue",
+        
+    )
 
     background_task.add_task(write_log, task.id, document_id)
 
+    crud.document.update_task_id(db, document, str(task.id))
+
     return {"msg": f"{task.id}"}
 
-@router.get('/{project_id}/docs/{document_id}/status/{task_id}')
+
+@router.get("/{project_id}/docs/{document_id}/status/{task_id}")
 def transcription_job_status(
     project_id: int,
     document_id: int,
     task_id: str,
     current_user: models.User = Depends(deps.get_current_active_superuser),
-    db: Session = Depends(deps.get_db)
-    ) -> str:
-    result = celery_app.AsyncResult(task_id)
+    db: Session = Depends(deps.get_db),
+) -> str:
+    result = celery_app.AsyncResult(id=task_id)
     return str(result.state)
